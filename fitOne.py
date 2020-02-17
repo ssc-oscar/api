a90a8bb3f0469e4952ec6c81ee9e8e28003ee0c2
Audris Mockus <audris@utk.edu>
2020-02-17 10:49:08 -0500

    add single-language fit

--- fitOne.py
+++ fitOne.py
@@ -14,33 +14,31 @@ nl = 0
 # First read into tags and ms
 sys.argv.pop(0)
 last=int(sys.argv.pop(0))
-lst="";
-while (len(sys.argv)):
- la = sys.argv.pop(0)
- lst=lst+la
- f=gzip.open("PtaPkgQ"+la+".s")
- for line in f:
-  l = line.rstrip().decode('ascii', 'ignore')
-  all = l.split(';')
-  p = all .pop(0)
-  t = all .pop(0)
-  if int(t) < last: 
-   a = all .pop(0)
-   m = all
-   tag = [ la, p, a ]
-   tags.append(tag)
-   ms.append(";".join(m))
-   nl+=1
+la=sys.argv.pop(0)
+lst=la;
+f=gzip.open("PtaPkgQ"+la+".s")
+for line in f:
+ l = line.rstrip().decode('ascii', 'ignore')
+ all = l.split(';')
+ p = all .pop(0)
+ t = all .pop(0)
+ if int(t) < last: 
+  a = all .pop(0)
+  m = all
+  tag = [ la, p, a ]
+  tags.append(tag)
+  ms.append(";".join(m))
+  nl+=1
 
 print ('records:' + str(nl))
 
 ldoc = namedtuple('ldoc', 'words tags split')
 docs = []
 for l in enumerate([ (w,t) for w,t in zip(ms, tags) ]):
-  words=l[1][0].split(";")
-  tags=l[1][1]
-  split=random.choice(['test','train','validate'])
-  docs.append(ldoc(words, tags, split))
+ words=l[1][0].split(";")
+ tags=l[1][1]
+ split=random.choice(['test','train','validate'])
+ docs.append(ldoc(words, tags, split))
 
 mod = Doc2Vec(dm=1, dbow_words=1, dm_mean=1, vector_size=100, window=2, negative=100, min_count=2, workers=cores)
 
@@ -50,8 +48,8 @@ dl = docs[:]
 #mod = Doc2Vec.load ("doc2vec.model1.13")
 
 for epoch in range(10):
-  mod.train(dl, epochs=mod.epochs, total_examples=mod.corpus_count)
-  mod.alpha -= 0.002
-  mod.min_alpha = mod.alpha
-  mod.save("doc2vec.Q"+lst+"."+str(last)+"."+str(epoch))
+ mod.train(dl, epochs=mod.epochs, total_examples=mod.corpus_count)
+ mod.alpha -= 0.002
+ mod.min_alpha = mod.alpha
+ mod.save("doc2vec.Q"+lst+"."+str(last)+"."+str(epoch))
 
